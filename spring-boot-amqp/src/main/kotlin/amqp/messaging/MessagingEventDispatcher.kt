412975c60ec5e353ebf9af6a2bcf5c4aee346277
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2018-07-12 19:13:46 +0200

    polishing AMQP example application

--- spring-boot-amqp/src/main/kotlin/amqp/messaging/MessagingEventDispatcher.kt
+++ spring-boot-amqp/src/main/kotlin/amqp/messaging/MessagingEventDispatcher.kt
@@ -1,19 +1,19 @@
 package amqp.messaging
 
 import org.springframework.amqp.core.TopicExchange
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
 import amqp.foo.FooEvent
 import amqp.foo.FooEventDispatcher
+import org.springframework.amqp.core.AmqpTemplate
 
 @Component
 class MessagingEventDispatcher(
-        private val rabbitTemplate: RabbitTemplate,
+        private val amqpTemplate: AmqpTemplate,
         @FooEvents private val exchange: TopicExchange
 ) : FooEventDispatcher {
 
     override fun dispatch(event: FooEvent) {
-        rabbitTemplate.convertAndSend(exchange.name, event.type, event)
+        amqpTemplate.convertAndSend(exchange.name, event.type, event)
     }
 
 }
