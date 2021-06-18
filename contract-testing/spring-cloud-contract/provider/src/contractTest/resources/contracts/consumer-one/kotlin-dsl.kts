603beb24f6440ad0c098cca92a2d6bc61cf2e18b
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-06-18 13:10:50 +0200

    added additional relaxed example

--- contract-testing/spring-cloud-contract/provider/src/contractTest/resources/contracts/consumer-one/kotlin-dsl.kts
+++ contract-testing/spring-cloud-contract/provider/src/contractTest/resources/contracts/consumer-one/kotlin-dsl.kts
@@ -32,7 +32,31 @@ arrayOf(
         }
     },
     contract {
-        name = "get single book by id - relaxed"
+        name = "get single book by id - relaxed 1"
+        request {
+            method = GET
+            urlPath = path("/books/b3fc0be8-463e-4875-9629-67921a1e00f4")
+            headers {
+                accept = APPLICATION_JSON
+            }
+        }
+        response {
+            status = OK
+            headers {
+                contentType = APPLICATION_JSON
+            }
+            body = body(
+                "isbn" to v(c("9780132350884"), p(regex("""\d{10}|\d{13}"""))),
+                "title" to v(c("Clean Code"), p(anyNonBlankString)),
+                "authors" to listOf(
+                    v(c("Robert C. Martin"), p(anyNonBlankString)),
+                    v(c("Dean Wampler"), p(anyNonBlankString))
+                )
+            )
+        }
+    },
+    contract {
+        name = "get single book by id - relaxed 2"
         request {
             method = GET
             urlPath = path("/books/b3fc0be8-463e-4875-9629-67921a1e00f4")
