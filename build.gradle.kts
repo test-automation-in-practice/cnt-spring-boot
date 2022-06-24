730887f4c259198000182532b59666d2963d5be7
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2022-06-24 12:09:24 +0200

    remove no longer needed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.0" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    id("org.springframework.cloud.contract") version "3.1.1" apply false
     id("org.asciidoctor.jvm.convert") version "3.3.2" apply false
 
     kotlin("jvm") version "1.6.21" apply false
