212d35ec236d6d3c3a70aa09420f7cd885a61b09
Stefan Ludwig <github@slu-it.de>
2023-06-02 18:30:51 +0200

    update dependencies, gradle config and add workaround for Kafka testing issue

--- examples/messaging-kafka/src/test/kotlin/example/spring/boot/kafka/messaging/MessagingIntegrationTests.kt
+++ examples/messaging-kafka/src/test/kotlin/example/spring/boot/kafka/messaging/MessagingIntegrationTests.kt
@@ -10,14 +10,19 @@ import example.spring.boot.kafka.events.PublishEventFunction
 import example.spring.boot.kafka.utils.InitializeWithContainerizedKafka
 import io.mockk.every
 import io.mockk.verify
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.kafka.core.KafkaTemplate
+import java.lang.Thread.sleep
 
+@TestInstance(PER_CLASS)
 @SpykBean(EventHandler::class)
 @InitializeWithContainerizedKafka
 @SpringBootTest(classes = [MessagingIntegrationTestsConfiguration::class])
@@ -29,6 +34,11 @@ class MessagingIntegrationTests(
 
     val createdEvent = Examples.cleanCode.createdEvent()
     val deletedEvent = Examples.cleanCode.deletedEvent()
+
+    @BeforeAll
+    fun waitForReadiness() {
+        sleep(1_000) // this is a new issue after Spring Boot 3.1 - never needed to wait before ...
+    }
 
     @Test
     fun `book created events are dispatched and received`() {
