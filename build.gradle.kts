177d331ddacc9f0d2672a0c80f82cc1535771932
Stefan Ludwig <github@slu-it.de>
2023-07-14 08:58:24 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.0" apply false
+    id("org.springframework.boot") version "3.1.1" apply false
     id("io.spring.dependency-management") version "1.1.0" apply false
     id("org.asciidoctor.jvm.convert") version "3.3.2" apply false
 
-    kotlin("jvm") version "1.8.21" apply false
-    kotlin("plugin.spring") version "1.8.21" apply false
-    kotlin("plugin.jpa") version "1.8.21" apply false
-    kotlin("plugin.noarg") version "1.8.21" apply false
+    kotlin("jvm") version "1.8.22" apply false
+    kotlin("plugin.spring") version "1.8.22" apply false
+    kotlin("plugin.jpa") version "1.8.22" apply false
+    kotlin("plugin.noarg") version "1.8.22" apply false
 }
 
 allprojects {
@@ -26,9 +26,9 @@ allprojects {
             imports {
                 mavenBom("io.github.logrecorder:logrecorder-bom:2.7.0")
                 mavenBom("io.github.openfeign:feign-bom:12.3")
-                mavenBom("org.jetbrains.kotlin:kotlin-bom:1.8.21")
+                mavenBom("org.jetbrains.kotlin:kotlin-bom:1.8.22")
                 mavenBom("org.testcontainers:testcontainers-bom:1.18.3")
-                mavenBom("org.zalando:logbook-bom:3.0.0")
+                mavenBom("org.zalando:logbook-bom:3.1.0")
 
                 mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.3")
                 mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
@@ -44,7 +44,7 @@ allprojects {
 
                 // legacy compatibility
                 dependency("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.7.0")
-                dependency("org.apache.activemq:artemis-jms-server:2.28.0")
+                dependency("org.apache.activemq:artemis-jms-server:2.29.0")
             }
         }
     }
