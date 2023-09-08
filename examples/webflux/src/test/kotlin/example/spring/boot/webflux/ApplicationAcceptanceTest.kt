26777f9e3476cfdbcb9a47df44b6ba136fefc804
Stefan Ludwig <github@slu-it.de>
2023-09-08 15:58:05 +0200

    add Logbook to WebFlux example

--- examples/webflux/src/test/kotlin/example/spring/boot/webflux/ApplicationAcceptanceTest.kt
+++ examples/webflux/src/test/kotlin/example/spring/boot/webflux/ApplicationAcceptanceTest.kt
@@ -1,11 +1,17 @@
 package example.spring.boot.webflux
 
+import io.github.logrecorder.api.LogRecord
+import io.github.logrecorder.assertion.LogRecordAssertion.Companion.assertThat
+import io.github.logrecorder.assertion.containsExactly
+import io.github.logrecorder.logback.junit5.RecordLoggers
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.zalando.logbook.Logbook
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 internal class ApplicationAcceptanceTest {
@@ -32,6 +38,21 @@ internal class ApplicationAcceptanceTest {
             .jsonPath("id").exists()
             .jsonPath("title").isEqualTo("Clean Code")
             .jsonPath("isbn").isEqualTo("9780132350884")
+    }
+
+    @Test
+    @RecordLoggers(Logbook::class)
+    fun `request and response are logged with obfuscated authorization header`(log: LogRecord) {
+        client.post()
+            .uri("/default-api/books")
+            .header(AUTHORIZATION, "some-token")
+            .contentType(APPLICATION_JSON)
+            .bodyValue("""{ "title": "Clean Code", "isbn": "9780132350884" }""")
+            .exchange()
+        assertThat(log) containsExactly {
+            trace(startsWith("Incoming Request:"), contains("authorization: XXX"))
+            trace(startsWith("Outgoing Response:"))
+        }
     }
 
 }
