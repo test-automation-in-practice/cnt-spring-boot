c16ae91fb6afd0eef0f1e69afe4ac9e0562c4f5d
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2023-11-08 23:54:15 +0100

    upgrade dependencies and tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.3" apply false
+    id("org.springframework.boot") version "3.1.5" apply false
     id("io.spring.dependency-management") version "1.1.3" apply false
     id("org.asciidoctor.jvm.convert") version "3.3.2" apply false
 
@@ -27,10 +27,10 @@ allprojects {
         the<DependencyManagementExtension>().apply {
             imports {
                 mavenBom("io.github.logrecorder:logrecorder-bom:2.7.0")
-                mavenBom("io.github.openfeign:feign-bom:12.5")
+                mavenBom("io.github.openfeign:feign-bom:13.0")
                 mavenBom("org.jetbrains.kotlin:kotlin-bom:1.8.22")
-                mavenBom("org.testcontainers:testcontainers-bom:1.19.0")
-                mavenBom("org.zalando:logbook-bom:3.4.0")
+                mavenBom("org.testcontainers:testcontainers-bom:1.19.1")
+                mavenBom("org.zalando:logbook-bom:3.6.0")
 
                 mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.4")
                 mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
@@ -38,8 +38,8 @@ allprojects {
             dependencies {
                 dependency("com.github.dasniko:testcontainers-keycloak:2.6.0")
                 dependency("com.ninja-squad:springmockk:4.0.2")
-                dependency("io.kotest:kotest-assertions-core:5.6.2")
-                dependency("io.mockk:mockk-jvm:1.13.7")
+                dependency("io.kotest:kotest-assertions-core:5.8.0")
+                dependency("io.mockk:mockk-jvm:1.13.8")
 
                 // currently using older version because of issues updating to Jakarta based version
                 dependency("org.apache.activemq:activemq-broker:5.17.3")
@@ -48,8 +48,8 @@ allprojects {
                 dependency("org.apache.activemq:activemq-kahadb-store:5.17.3")
 
                 // legacy compatibility
-                dependency("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.9.2")
-                dependency("org.apache.activemq:artemis-jms-server:2.28.0")
+                dependency("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.10.1")
+                dependency("org.apache.activemq:artemis-jms-server:2.31.2")
             }
         }
     }
