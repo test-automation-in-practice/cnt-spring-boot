5a4b1fc0cc693a1af50a88057d04516a1303afa9
Stefan Ludwig <slu@novatec-gmbh.de>
2024-04-12 14:03:50 +0200

    optimize rabbitmq vhost creation

--- examples/messaging-rabbitmq/src/test/kotlin/example/spring/boot/rabbitmq/utils/InitializeWithContainerizedRabbitMQ.kt
+++ examples/messaging-rabbitmq/src/test/kotlin/example/spring/boot/rabbitmq/utils/InitializeWithContainerizedRabbitMQ.kt
@@ -5,13 +5,6 @@ import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.support.TestPropertySourceUtils.addInlinedPropertiesToEnvironment
 import org.testcontainers.containers.RabbitMQContainer
-import java.net.Authenticator
-import java.net.PasswordAuthentication
-import java.net.URI.create
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpRequest.BodyPublishers.noBody
-import java.net.http.HttpResponse.BodyHandlers.discarding
 import java.util.UUID.randomUUID
 import kotlin.annotation.AnnotationTarget.CLASS
 
@@ -24,13 +17,8 @@ class RabbitMQInitializer : ApplicationContextInitializer<ConfigurableApplicatio
 
     companion object {
         private val container: RabbitMQContainer by lazy {
-            RabbitMQContainer("rabbitmq:3.13-management")
+            RabbitMQContainer("rabbitmq:3.13")
                 .apply { start() }
-        }
-        private val httpClient: HttpClient by lazy {
-            HttpClient.newBuilder()
-                .authenticator(BasicAuthenticator(container.adminUsername, container.adminPassword))
-                .build()
         }
     }
 
@@ -49,15 +37,10 @@ class RabbitMQInitializer : ApplicationContextInitializer<ConfigurableApplicatio
 
     private fun createVirtualHost(): String {
         val virtualHost = "${randomUUID()}"
-        val request = HttpRequest.newBuilder()
-            .PUT(noBody())
-            .uri(create("http://${container.host}:${container.httpPort}/api/vhosts/$virtualHost"))
-            .build()
-        httpClient.send(request, discarding())
+        with(container) {
+            execInContainer("rabbitmqctl", "add_vhost", virtualHost)
+            execInContainer("rabbitmqctl", "set_permissions", "--vhost", virtualHost, adminUsername, ".*", ".*", ".*")
+        }
         return virtualHost
-    }
-
-    private class BasicAuthenticator(val username: String, val password: String) : Authenticator() {
-        override fun getPasswordAuthentication() = PasswordAuthentication(username, password.toCharArray())
     }
 }
