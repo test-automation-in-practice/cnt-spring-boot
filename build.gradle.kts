6fb75a32cd4a724d2f351afead1d3bdbb7c3664e
Stefan Ludwig <github@slu-it.de>
2025-07-26 16:09:41 +0200

    upgrade Spring Boot and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,17 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 
 plugins {
-    id("org.springframework.boot") version "3.4.0" apply false
-    id("io.spring.dependency-management") version "1.1.6" apply false
+    id("org.springframework.boot") version "3.5.4" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
     id("org.asciidoctor.jvm.convert") version "3.3.2" apply false
 
-    kotlin("jvm") version "2.0.21" apply false
-    kotlin("plugin.spring") version "2.0.21" apply false
-    kotlin("plugin.jpa") version "2.0.21" apply false
-    kotlin("plugin.noarg") version "2.0.21" apply false
+    kotlin("jvm") version "2.1.21" apply false
+    kotlin("plugin.spring") version "2.1.21" apply false
+    kotlin("plugin.jpa") version "2.1.21" apply false
+    kotlin("plugin.noarg") version "2.1.21" apply false
 }
 
 allprojects {
@@ -31,19 +32,20 @@ allprojects {
         the<DependencyManagementExtension>().apply {
             imports {
                 mavenBom("io.github.logrecorder:logrecorder-bom:2.10.0")
-                mavenBom("io.github.openfeign:feign-bom:13.3")
-                mavenBom("org.jetbrains.kotlin:kotlin-bom:2.0.21")
+                mavenBom("io.github.openfeign:feign-bom:13.6")
+                mavenBom("org.jetbrains.kotlin:kotlin-bom:2.1.21")
                 mavenBom("org.testcontainers:testcontainers-bom:1.20.1")
-                mavenBom("org.zalando:logbook-bom:3.9.0")
+                mavenBom("org.zalando:logbook-bom:3.12.2")
 
-                mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0-RC1") // TODO update
-                mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+                mavenBom("org.springframework.modulith:spring-modulith-bom:1.4.2")
+                mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
+                mavenBom(BOM_COORDINATES)
             }
             dependencies {
                 dependency("com.github.dasniko:testcontainers-keycloak:2.6.0")
                 dependency("com.ninja-squad:springmockk:4.0.2")
                 dependency("io.kotest:kotest-assertions-core:5.9.1")
-                dependency("io.mockk:mockk-jvm:1.13.13")
+                dependency("io.mockk:mockk-jvm:1.14.5")
                 dependency("com.squareup.okhttp3:okhttp:4.12.0")
             }
         }
