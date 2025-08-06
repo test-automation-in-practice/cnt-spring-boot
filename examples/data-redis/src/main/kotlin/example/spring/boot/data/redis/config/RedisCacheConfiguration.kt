365ae7dfabbbc0082c39560c4248ad6e06500327
Stefan Ludwig <github@slu-it.de>
2025-08-06 22:42:25 +0200

    add error handler to Redis example
    connection issues will now be logged and the cache skipped

--- examples/data-redis/src/main/kotlin/example/spring/boot/data/redis/config/RedisCacheConfiguration.kt
+++ examples/data-redis/src/main/kotlin/example/spring/boot/data/redis/config/RedisCacheConfiguration.kt
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.interceptor.CacheErrorHandler
+import org.springframework.cache.interceptor.LoggingCacheErrorHandler
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig
 import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder
@@ -25,5 +28,8 @@ class RedisCacheConfiguration : RedisCacheManagerBuilderCustomizer {
             )
             .disableCreateOnMissingCache()
     }
+
+    @Bean // if Redis is not available, this will just log an error and skip the cache
+    fun cacheErrorHandler(): CacheErrorHandler = LoggingCacheErrorHandler(true)
 
 }
